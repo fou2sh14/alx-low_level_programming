1091ce8795ee908a9f22b79f9f2c460042a410b2
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-20 10:50:08 -0700

    FGFG

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A c program to print a statement with puts function
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
