76740ab049674106b217fcb640f5093d8dbe0cf4
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-20 11:04:54 -0700

    FGFDGFDG

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A c program to print a statement with printf function
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
