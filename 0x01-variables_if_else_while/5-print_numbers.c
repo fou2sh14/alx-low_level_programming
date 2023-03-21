cc5a596988ce783c78a3d015af10a3b851fe81c2
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-20 19:59:56 -0700

    FGHFDHFDH

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (int i == 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
