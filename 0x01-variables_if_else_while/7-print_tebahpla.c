231506a340507c41c7d7568f237ea2969abfeb3b
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-21 00:52:18 -0700

    HGHG

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print reverse numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; i--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
