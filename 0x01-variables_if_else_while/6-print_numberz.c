1b1c6f5581487b40072a9762a984e14089c23e1d
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-20 20:17:16 -0700

    FHFFHFHF

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers by putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('i');
+	}
+	putchar('\n');
+	return (0);
+}
