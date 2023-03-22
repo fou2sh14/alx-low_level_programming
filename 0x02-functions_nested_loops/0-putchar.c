2e082e6af75396c24b03da0443e71e2580aa2107
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-22 16:33:14 -0700

    FHFDHFD

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+}
+
