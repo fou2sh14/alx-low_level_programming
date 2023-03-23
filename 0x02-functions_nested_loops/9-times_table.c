fd2c4013677f716671d2bcc3063df21b84ee6389
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-23 08:38:22 -0700

    GJJ

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,8 @@ void times_table(void)
 			if (c <= 9)
 				_putchar(' ');
 			else
-				_putchar((c % 10) + 48);
+				_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
 		}
 		_putchar('\n');
 	}
