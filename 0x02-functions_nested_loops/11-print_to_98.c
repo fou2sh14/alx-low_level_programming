ec38ff8f112790889ead9f095b88b782af917c7b
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-23 11:25:13 -0700

    GHGH

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		for (i = n; n < 98; i++)
 		{
-			_putchar(i);
+			_putchar(i + 48);
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	{
 		for (i = n; n > 98; i--)
 		{
-			_putchar(i);
+			_putchar(i + 48);
 			_putchar(',');
 			_putchar(' ');
 		}
