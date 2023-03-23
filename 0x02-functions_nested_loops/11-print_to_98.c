58de0b56ed7b16e06736d50adb918b4d5ed1f195
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-23 11:35:55 -0700

    GJGFJGFJ

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; n < 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			_putchar(i + 48);
 			_putchar(',');
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; n > 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			_putchar(i + 48);
 			_putchar(',');
