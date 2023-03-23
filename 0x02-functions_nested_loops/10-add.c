28f0e7147a70d642a407118cc1513024ed83e92d
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-23 08:46:11 -0700

    GJGFJGFJGFJ

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds to numbers
+ *
+ * @a: takes the first input from function
+ *
+ * @b: takes the second input from function
+ *
+ * Return: Always return s
+ */
+
+int add(int a, int b)
+{
+	int s = a + b;
+
+	return (s);
+}
