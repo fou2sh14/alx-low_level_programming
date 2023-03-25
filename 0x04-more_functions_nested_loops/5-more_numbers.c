eb912023eecfc0196a7ec41bcd56a788dc92a2ac
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-24 19:51:57 -0700

    FHHFH

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, count;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			count = j;
 			if (j > 9)
 			{
 				_putchar(1 + 48);
-				j = j % 10;
+				count = j % 10;
 			}
-			_putchar(j + 48);
+			_putchar(count + 48);
 		}
 		_putchar('\n');
 	}
