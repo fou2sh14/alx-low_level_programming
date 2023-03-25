0dba3010c4fa85394bf5c8a48671405ed9f7b8c2
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-24 19:09:19 -0700

    GHJGFHJGFJHG

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiple two integers
+ *
+ * @a: takes first input of function
+ *
+ * @b: takes second input of function
+ *
+ * Return: Always c
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
