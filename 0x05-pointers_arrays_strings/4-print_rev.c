385cf3686926d094a64e57ecf7a2a452fd8b3f35
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-28 18:31:11 -0700

    GJGJ

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse string
+ *
+ * @s: takes input
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
