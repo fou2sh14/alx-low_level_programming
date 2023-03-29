58d73d1a4976c37360540556b4c02c740836f16b
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-03-28 19:44:35 -0700

    GJHGJ

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
-/***/
+/**
+ * puts2 - put
+ *
+ * @str: input
+ */
 
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; )
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
