a6d2c5b03c39c58f589febca18380e2eef923f03
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-11 19:24:35 -0700

    GJHGJ

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -11,5 +12,6 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 int _putchar(char c);
+
 
 #endif
