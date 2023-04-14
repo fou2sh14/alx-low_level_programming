354a5ddb73845a209e1b768f3b77f8babb9f1dbe
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-13 20:12:39 -0700

    GJHGFH

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (if new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -48,7 +48,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		for (f = 0; f < old_size; f++)
 		{
-			m[f] = *((char *)ptr + 1);
+			*((char *)m + f) = *((char *)ptr + f);
 		}
 		free(ptr);
 	}
