49ccccdcd90a4c9d54e6c7e9fa71b30e2fdaebce
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-16 11:29:46 -0700

    GGJGJJ

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,11 +19,19 @@ int length(char *str)
 	return (x);
 }
 
-/***/
+/**
+ * argstostr - function
+ *
+ * @ac: first input
+ *
+ * @av: second input
+ *
+ * Return: pointer
+ */
 
 char *argstostr(int ac, char **av)
 {
-	int f, g, h, d;
+	int f, g, h, d = 0;
 	char *m;
 
 	if (ac == 0 || av == NULL)
@@ -32,6 +40,25 @@ char *argstostr(int ac, char **av)
 	}
 	for (f = 0; f < ac; f++, g++)
 	{
-		g ++www
+		g += length(av[f]);
 	}
+	m = malloc(sizeof(char) * g + 1);
+
+	if (m == 0)
+	{
+		return (NULL);
+	}
+
+	for (f = 0; f < ac; f++)
+	{
+		for (h = 0; av[f][h] != '\0'; h++, d++)
+		{
+			m[d] = av[f][h];
+		}
+		m[d] = '\n';
+		d++;
+	}
+	m[d] = '\0';
+
+	return (m);
 }
