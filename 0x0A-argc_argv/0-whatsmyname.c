97aa069beab6f68a68af1ba3d594f41a17f2758f
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-15 18:23:36 -0700

    GJGJGJ

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print name of file
+ *
+ * @argc: number of arg
+ *
+ * @argv: array carry the arg
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
