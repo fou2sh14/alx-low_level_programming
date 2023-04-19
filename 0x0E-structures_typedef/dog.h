6a38910d5fb06e1073c3e9db8b686ef8dfedd4aa
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-18 17:31:18 -0700

    FGFGG

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+
+/**
+ * struct dog - a new data type called dog
+ *
+ * @name: first member
+ *
+ * @age: second member
+ *
+ * @owner: third member
+ *
+ * Description: its a new data type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
