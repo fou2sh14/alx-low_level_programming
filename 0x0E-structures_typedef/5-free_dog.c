9c04068d801b17c3c9ebd4a1180a6adec0b12dab
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-18 19:46:25 -0700

    HGHGG

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free all dogs
+ *
+ * @d: the dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+
+		free(d);
+	}
+}
