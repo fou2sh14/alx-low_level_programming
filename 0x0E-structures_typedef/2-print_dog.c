8347d110a5333117261e9177d8aa30e46e4000f9
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-18 19:05:29 -0700

    H

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,25 +11,25 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if ((*d).name)
+		if (d->name)
 		{
-			printf("Name: %s\n", (*d).name);
+			printf("Name: %s\n", d->name);
 		}
 		else
 		{
 			printf("Name: %S\n", "(nil)");
 		}
-		if ((*d).age)
+		if (d->age)
 		{
-			printf("Age: %f\n", (*d).age);
+			printf("Age: %f\n", d->age);
 		}
 		else
 		{
 			printf("Age: %s\n", "(nil)");
 		}
-		if ((*d).owner)
+		if (d->owner)
 		{
-			printf("Owner: %s\n", (*d).owner);
+			printf("Owner: %s\n", d->owner);
 		}
 		else
 		{
