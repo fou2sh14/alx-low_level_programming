57d7a35b3d46be180f8d14c25a857d389a1422d3
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-18 18:04:25 -0700

    GJGJ

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable
+ *
+ * @d: first input
+ *
+ * @name: first member
+ *
+ * @age: second member
+ *
+ * @owner: third member
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
