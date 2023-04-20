5ec34c527cbd6bffcb06fe8d82a627b605f3e223
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-19 19:45:07 -0700

    GHG

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * array-iterator - function call function
+ *
+ * @array: the array
+ *
+ * @size: the length
+ *
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *f = array + (size - 1);
+
+	if (size && array && action)
+	{
+		while (array <= f)
+		{
+			action(*array);
+			array++;
+		}
+	}
+}
