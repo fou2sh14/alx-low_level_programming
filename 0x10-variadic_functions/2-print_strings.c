917480655f02f102d3bba08bd38161f3f24fef4a
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-20 19:50:21 -0700

    FGHH

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,8 +25,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	while (f--)
 	{
-		printf("%s%S", (string = va_arg(lst, char *)) ? string : "(nil)",
-				f ? (separator ? separator : "") : "\n");
+		printf("%s%s", (string = va_arg(lst, char *)) ? string : "(nil)",
+				(f ? (separator ? separator : "") : "\n"));
 	}
 
 	va_end(lst);
