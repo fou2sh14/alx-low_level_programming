f916b0a07bee8c22eef06176751cd153c2c5a8db
Fouad_Gabr19 <foadjabr14@gmail.com>
2023-04-20 19:53:43 -0700

    GH

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print anything
+ *
+ * @format: the format
+ *
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	;
+}
