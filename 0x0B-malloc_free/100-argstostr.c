7af3a9e3b6e4974072209ce81ca9edf8fb094876
Fouad_Gabr16 <foadjabr14@gmail.com>
2023-07-11 20:22:05 -0700

    HGGHG

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * length - size
+ *
+ * @s: string
+ *
+ * Return: size of string
+ */
+
+int length(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
+
+/**
+ * argstostr - conccat all arguments
+ *
+ * @ac: number of arguments
+ *
+ * @av: the arguments
+ *
+ * Return: concating arguments
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *string;
+	int a, b, c, d;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++, b++)
+	{
+		b += length(av[a]);
+	}
+
+	string = malloc(sizeof(char) * b + 1);
+
+	if (string == 0)
+		return (NULL);
+
+	for (a = 0; a < ac; a++)
+	{
+		for (c = 0; av[a][c] != '\0'; c++, d++)
+		{
+			string[d] = av[a][c];
+		}
+		string[d] = '\n';
+		d++;
+	}
+	string[d] = '\0';
+	return (string);
+}
