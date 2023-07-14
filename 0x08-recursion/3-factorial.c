843ea6d9fdd8ea1128039918d18376027478f290
Fouad_Gabr16 <foadjabr14@gmail.com>
2023-07-14 11:51:31 -0700

    FHGFHF

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - fact function
+ *
+ * @n: number
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
