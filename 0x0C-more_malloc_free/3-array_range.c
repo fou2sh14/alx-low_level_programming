b714444c3ed41fd8c6576c124431de0603817bb2
Fouad_Gabr16 <foadjabr14@gmail.com>
2023-07-14 05:09:19 -0700

    FGHFDHHD

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * array_range - create array of min to max
+ *
+ * @min: minimum
+ *
+ * @max: maximum
+ *
+ * Return: pointer
+ */
+
+int *array_range(int min, int max)
+{
+	int *m;
+	int f;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	m = malloc(sizeof(int) * ((max - min) + 1));
+	if (m == 0)
+	{
+		return (NULL);
+	}
+
+	for (f = min; f <= max; f++)
+	{
+		m[f - min] = f;
+	}
+	m[f] = '\0';
+	return (m);
+}
