ea7c54ef3a8dfe4f45859e1cbfd2ee24f436ad29
Fouad_Gabr16 <foadjabr14@gmail.com>
2023-07-13 19:52:47 -0700

    FGFGFDGFDGD

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * set - like memset
+ *
+ * @s: string
+ *
+ * @a: second
+ *
+ * @x: third
+ *
+ * Return: pointer
+ */
+
+char *set(char *s, char a, unsigned int x)
+{
+	char *point = s;
+
+	while (x--)
+	{
+		*s++ = a;
+	}
+	return (point);
+}
+
+/**
+ * _calloc - like calloc
+ *
+ * @nmemb: first input
+ *
+ * @size: size
+ *
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+
+	set(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
