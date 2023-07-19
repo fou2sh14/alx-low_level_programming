7a62d1ae51e0329540e3ee946b008f377dc7520f
Fouad_Gabr16 <foadjabr14@gmail.com>
2023-07-18 17:59:59 -0700

    HG

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type
+ *
+ * @name: first member
+ *
+ * @age: second
+ *
+ * @owner: third
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
