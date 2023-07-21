796c162a6348abf924c28fb6a6a1d282961c30ca
Fouad_Gabr16 <foadjabr14@gmail.com>
2023-07-20 19:31:25 -0700

    GHGHGHH

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function sum
+ *
+ * @n: num of arg
+ *
+ * Return: summ
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int f;
+	int summ = 0;
+	va_list list;
+
+	va_start(list, n);
+
+	for (f = 0; f < n; f++)
+	{
+		summ += va_arg(list; int);
+	}
+	va_end(list);
+
+	return (summ);
+}
