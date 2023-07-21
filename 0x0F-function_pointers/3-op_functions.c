1b55c01bb3ff86d10c34b62223467b7238d969d6
Fouad_Gabr16 <foadjabr14@gmail.com>
2023-07-20 18:45:52 -0700

    GHGHH

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum function
+ *
+ * @a: first num
+ *
+ * @b: second num
+ *
+ * Return: sum of them
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract fun
+ *
+ * @a: first num
+ *
+ * @b: second num
+ *
+ * Return: substraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply fun
+ *
+ * @a: first num
+ *
+ * @b: second num
+ *
+ * Return: multiply
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ *
+ * @a: first
+ *
+ * @b: second
+ *
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ *
+ * @a: first
+ *
+ * @b: second
+ *
+ * Return: modulus
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
