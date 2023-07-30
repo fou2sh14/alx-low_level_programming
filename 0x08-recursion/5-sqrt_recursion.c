4dd996b5c10a99cc4ddc9840d574a3be10567e87
Fouad_Gabr45 <foadjabr14@gmail.com>
2023-07-30 13:20:42 -0700

    GHGH

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+int sqrt(int n, int y);
+
+/**
+ * _sqrt_recursion - get square root
+ *
+ * @n: number
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
+
+/**
+ * sqrt - get square root function
+ *
+ * @n: number
+ *
+ * @y: counter
+ *
+ * Return: int
+ */
+
+int sqrt(int n, int y)
+{
+	if (y * y == n)
+	{
+		return (y);
+	}
+	else if (y * y < n)
+	{
+		return (sqrt(n, y + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
