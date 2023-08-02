084ce22cc2896b65f143a0ed6d0881213ef46623
Fouad_Gabr45 <foadjabr14@gmail.com>
2023-08-01 18:18:10 -0700

    FHGFH

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add begining
+ *
+ * @head: head
+ *
+ * @n: data
+ *
+ * Return: new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (!head || !new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+
+	if (*head)
+	{
+		new->next = *head;
+	}
+	*head = new;
+
+	return (new);
+}
