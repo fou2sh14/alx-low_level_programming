acb32089e3aeb34a580c89b0d80def1e4b644c50
Fouad_Gabr45 <foadjabr14@gmail.com>
2023-08-02 13:31:50 -0700

    GHGH

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: number of args
+ *
+ * @argv: array of args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
